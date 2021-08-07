44bbb6393dbdd59066c01f15f8ac333ad223632b
Chris Woelk <chris@redbeed.com>
2021-08-07 12:21:09 +0200

    Update follower & subscriber after login

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,12 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Events\Login;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
+use Redbeed\OpenOverlay\Listeners\UpdateTwitchUserFollowers;
+use Redbeed\OpenOverlay\Listeners\UpdateTwitchUserSubscriber;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -17,6 +19,10 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        Login::class => [
+            UpdateTwitchUserSubscriber::class,
+            UpdateTwitchUserFollowers::class
         ],
     ];
 
