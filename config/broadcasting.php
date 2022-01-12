10d9ac17cd89be6d94df856f968cd6cf11911c6a
Chris Woelk <chris@redbeed.com>
2022-01-12 15:29:03 +0100

    Add more options to broadcaster

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -39,7 +39,7 @@ return [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
                 'host' => env('PUSHER_APP_HOST', '127.0.0.1'),
-                'port' => 6001,
+                'port' => env('PUSHER_APP_PORT', 6001),
                 'scheme' => 'http',
             ],
         ],
